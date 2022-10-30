325cbcdb75e24ce94e269f653f9add89a2d01d02
Szalay Tamás <szalaytamas90@gmail.com>
2022-10-30 21:58:14 +0100

    supplement location and keyword validation

--- workforce/src/main/java/dev/workforce/workforce/controller/ClientController.java
+++ workforce/src/main/java/dev/workforce/workforce/controller/ClientController.java
@@ -5,6 +5,7 @@ import dev.workforce.workforce.service.ClientService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/client")
+@Validated
 public class ClientController {
     private ClientService clientService;
 
--- workforce/src/main/java/dev/workforce/workforce/controller/PositionController.java
+++ workforce/src/main/java/dev/workforce/workforce/controller/PositionController.java
@@ -7,10 +7,12 @@ import dev.workforce.workforce.service.ClientService;
 import dev.workforce.workforce.service.PositionService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import javax.validation.Valid;
+import javax.validation.constraints.Size;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -18,6 +20,7 @@ import java.util.Optional;
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/position")
+@Validated
 public class PositionController {
     private PositionService positionService;
     private ClientService clientService;
@@ -37,9 +40,12 @@ public class PositionController {
 
     @Operation(summary = "by a keyword and a location find all the fitting positions url")
     @GetMapping("/search")
-    public List<URI> findJobWithRequirements(@RequestParam String keyword, @RequestParam String location, @RequestParam String apikey) {
+    public List<URI> findJobWithRequirements(
+            @RequestParam @Size(max = 50, message = "keyword cant be longer than 50 character") String keyword,
+            @RequestParam @Size(max = 50, message = "location cant be longer than 50 character") String location,
+            @RequestParam String apikey) {
         clientService.validateApiKey(apikey);
-        return positionService.findPositionWhereFitsToCriterias(keyword,location);
+        return positionService.findPositionWhereFitsToCriterias(keyword, location);
 
     }
 }
